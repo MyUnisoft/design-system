19fd2493a7776801cb1f7c06399dff7444885a6d
Ludovic Bekaert <l.bekaert@myunisoft.fr>
2022-01-07 14:48:16 +0100

    Deploy website - based on 470e85e9c667887f00b31aa96800811164cceac6

--- assets/js/react-syntax-highlighter_languages_refractor_hsts.868ca909.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[3140],{60561:function(e){"use strict";function a(e){e.languages.hsts={directive:{pattern:/\b(?:max-age=|includeSubDomains|preload)/,alias:"keyword"},safe:{pattern:/\d{8,}/,alias:"selector"},unsafe:{pattern:/\d{1,7}/,alias:"function"}}}e.exports=a,a.displayName="hsts",a.aliases=[]}}]);
--- assets/js/react-syntax-highlighter_languages_refractor_ini.07da3e6d.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[6495],{14970:function(t){"use strict";function e(t){t.languages.ini={comment:/^[ \t]*[;#].*$/m,selector:/^[ \t]*\[.*?\]/m,constant:/^[ \t]*[^\s=]+?(?=[ \t]*=)/m,"attr-value":{pattern:/=.*/,inside:{punctuation:/^[=]/}}}}t.exports=e,e.displayName="ini",e.aliases=[]}}]);
