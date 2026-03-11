ffcfbb9e37a23a6b229645c59afb8e194df050f6
Adrien <a.richard@myunisoft.fr>
2026-03-11 14:38:07 +0100

    Deploy website - based on 786b78523e588e0a9d38c19e23a6005e90dfe779

---
+++ assets/js/react-syntax-highlighter_languages_refractor_hsts.cf90cd93.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkwebsite=globalThis.webpackChunkwebsite||[]).push([[452],{20116(a){function e(a){a.languages.hsts={directive:{pattern:/\b(?:max-age=|includeSubDomains|preload)/,alias:"keyword"},safe:{pattern:/\d{8,}/,alias:"selector"},unsafe:{pattern:/\d{1,7}/,alias:"function"}}}a.exports=e,e.displayName="hsts",e.aliases=[]}}]);
---
+++ assets/js/react-syntax-highlighter_languages_refractor_ini.9bf3c785.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkwebsite=globalThis.webpackChunkwebsite||[]).push([[2638],{97888(t){function e(t){t.languages.ini={comment:/^[ \t]*[;#].*$/m,selector:/^[ \t]*\[.*?\]/m,constant:/^[ \t]*[^\s=]+?(?=[ \t]*=)/m,"attr-value":{pattern:/=.*/,inside:{punctuation:/^[=]/}}}}t.exports=e,e.displayName="ini",e.aliases=[]}}]);
